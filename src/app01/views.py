399054ba9d3d6e40b0e5f57478f00a6d4aa96d19
dy <2711099209@qq.com>
2016-06-14 18:32:38 +0800

    Django加载模板文件

--- src/app01/views.py
+++ src/app01/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render_to_response
-from django.http.response import HttpResponse
 
 # Create your views here.
-def index(req, uid,name):
+def index(req, uid, name):
     return render_to_response('index.html', {'title':'my page', 'uid':uid, 'name':name})
-#     str='<html><title></title><body>id:'+uid+'name:'+name+'</body></html>'
-#     return HttpResponse(str)
